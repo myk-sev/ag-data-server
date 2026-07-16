0fb913e305abb9bff7b13b3145372c984263af40
jake-ciliberti <jakeciliberti@gmail.com>
2026-07-16 10:21:11 -0400

    added sensor names and removed index writing

--- csv_cat.py
+++ csv_cat.py
@@ -1,13 +1,16 @@
 import argparse
-import csv
 import pandas as pd
+from pathlib import Path
 
 def concatenate() -> DataFrame:
     # read files to list
     csv_files = []
 
     for i in args.input:
-        csv_files.append(pd.read_csv(i, index_col=None, header=0))
+        csv_files.append(pd.read_csv(i, index_col=False, header=0))
+
+        if 'Sensor name' not in csv_files[-1].columns:
+            csv_files[-1].loc[:, "Sensor name"] = Path(i).name.split('.')[0]
 
     frame = pd.concat(csv_files, axis=0, ignore_index=True)
 
@@ -20,4 +23,4 @@ args = parser.parse_args()
 
 new_data = concatenate()
 
-new_data.to_csv(args.output[0])
+new_data.to_csv(args.output[0], index=False)
