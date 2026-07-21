a09e44c215bd096327c0ba8362349b2c4552ad1a
jake-ciliberti <jakeciliberti@gmail.com>
2026-07-21 09:36:30 -0400

    improved time conversion and added duplicate checking

--- csv_cat.py
+++ csv_cat.py
@@ -8,15 +8,14 @@ def concatenate() -> DataFrame:
     for i in args.input:
         new_file: DataFrame = pd.read_csv(i, index_col=False, header=0)
 
-        # FIX: drop old time columns
         # TODO: specify pre-conversion time formats
         if 'Timestamp for sample frequency every 1 min min' in new_file.columns:
             new_file["Time"] = pd.to_datetime(new_file['Timestamp for sample frequency every 1 min min'])
-            new_file.drop(columns=['Timestamp for sample frequency every 1 min min'])
+            new_file.drop(columns=['Timestamp for sample frequency every 1 min min'], inplace=True)
 
         if 'Time(DD/MM/YYYY h:mm:ss A)' in new_file.columns:
             new_file["Time"] = pd.to_datetime(new_file['Time(DD/MM/YYYY h:mm:ss A)'])
-            new_file.drop(columns=['Time(DD/MM/YYYY h:mm:ss A)'])
+            new_file.drop(columns=['Time(DD/MM/YYYY h:mm:ss A)'], inplace=True)
 
         if 'Sensor name' not in new_file.columns:
             new_file.loc[:, "Sensor name"] = Path(i).name.split('.')[0]
@@ -24,6 +23,8 @@ def concatenate() -> DataFrame:
         csv_files.append(new_file)
 
     frame = pd.concat(csv_files, axis=0, ignore_index=True)
+
+    frame.drop_duplicates(subset=["Time", "Sensor name"], inplace=True)
 
     return frame
 
